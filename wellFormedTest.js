7e9f7e90c15afff27f037f1172b2d597eba42d70
HalJune1026 <jh.dev.acc.1026.i2k9@gmail.com>
2023-04-29 15:58:06 +0900

    initial commit

---
+++ wellFormedTest.js
@@ -0,0 +1,9 @@
+
+console.log("あ".isWellFormed());
+
+console.log("\uD842".isWellFormed());
+
+console.log("あ".toWellFormed());
+
+console.log("\uD842".toWellFormed());
+console.log("\uD842".toWellFormed().isWellFormed());
